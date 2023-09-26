fdda89cf1081f6f8f2833bc88f6fcd3b77b09295
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2023-09-26 21:36:07 +0800

    Update

--- FCLauncher/src/main/jni/glfw/osmesa_context.c
+++ FCLauncher/src/main/jni/glfw/osmesa_context.c
@@ -2,9 +2,15 @@
 // Created by Tungsten on 2022/10/11.
 //
 
-#include <stdlib.h>
-#include <string.h>
 #include <assert.h>
+#include <EGL/egl.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <android/dlext.h>
+#include <nsbypass.h>
+#include "gl/gl.h"
 
 #include <internal.h>
 #include <android/native_window.h>
@@ -14,6 +20,13 @@ void (*vtest_swap_buffers) (void);
 
 ANativeWindow_Buffer buf;
 int32_t stride;
+
+#ifndef POJAVLAUNCHER_NSBYPASS_H
+#define POJAVLAUNCHER_NSBYPASS_H
+
+void* loadTurnipVulkan();
+
+#endif
 
 void* makeContextCurrentEGL(void* win) {
     _GLFWwindow* window = win;
@@ -120,7 +133,7 @@ static void swapBuffersOSMesa(_GLFWwindow* window)
     if (strcmp(getenv("LIBGL_STRING"), "VirGLRenderer") == 0) {
         window->context.Finish();
         vtest_swap_buffers();
-    } else {
+    } else if (strcmp(getenv("LIBGL_STRING"), "Zink") == 0) {
         OSMesaContext context = OSMesaGetCurrentContext();
         if (context == NULL) {
             printf("Zink: attempted to swap buffers without context!");
@@ -150,6 +163,98 @@ static int extensionSupportedOSMesa(const char* extension)
 //////////////////////////////////////////////////////////////////////////
 //////                       GLFW internal API                      //////
 //////////////////////////////////////////////////////////////////////////
+
+//#define ADRENO_POSSIBLE
+#ifdef ADRENO_POSSIBLE
+//Checks if your graphics are Adreno. Returns true if your graphics are Adreno, false otherwise or if there was an error
+bool checkAdrenoGraphics() {
+    EGLDisplay eglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if(eglDisplay == EGL_NO_DISPLAY || eglInitialize(eglDisplay, NULL, NULL) != EGL_TRUE) return false;
+    EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_SURFACE_TYPE, EGL_PBUFFER_BIT, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE };
+    EGLint num_configs = 0;
+    if(eglChooseConfig(eglDisplay, egl_attributes, NULL, 0, &num_configs) != EGL_TRUE || num_configs == 0) {
+        eglTerminate(eglDisplay);
+        return false;
+    }
+    EGLConfig eglConfig;
+    eglChooseConfig(eglDisplay, egl_attributes, &eglConfig, 1, &num_configs);
+    const EGLint egl_context_attributes[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE };
+    EGLContext context = eglCreateContext(eglDisplay, eglConfig, EGL_NO_CONTEXT, egl_context_attributes);
+    if(context == EGL_NO_CONTEXT) {
+        eglTerminate(eglDisplay);
+        return false;
+    }
+    if(eglMakeCurrent(eglDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, context) != EGL_TRUE) {
+        eglDestroyContext(eglDisplay, context);
+        eglTerminate(eglDisplay);
+    }
+    const char* vendor = glGetString(GL_VENDOR);
+    const char* renderer = glGetString(GL_RENDERER);
+    bool is_adreno = false;
+    if(strcmp(vendor, "Qualcomm") == 0 && strstr(renderer, "Adreno") != NULL) {
+        is_adreno = true; // TODO: check for Turnip support
+    }
+    eglMakeCurrent(eglDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+    eglDestroyContext(eglDisplay, context);
+    eglTerminate(eglDisplay);
+    return is_adreno;
+}
+void* load_turnip_vulkan() {
+    if(!checkAdrenoGraphics()) return NULL;
+    const char* native_dir = getenv("FCL_NATIVEDIR");
+    const char* cache_dir = getenv("TMPDIR");
+    if(!linker_ns_load(native_dir)) return NULL;
+    void* linkerhook = linker_ns_dlopen("liblinkerhook.so", RTLD_LOCAL | RTLD_NOW);
+    if(linkerhook == NULL) return NULL;
+    void* turnip_driver_handle = linker_ns_dlopen("libvulkan_freedreno.so", RTLD_LOCAL | RTLD_NOW);
+    if(turnip_driver_handle == NULL) {
+        printf("AdrenoSupp: Failed to load Turnip!\n%s\n", dlerror());
+        dlclose(linkerhook);
+        return NULL;
+    }
+    void* dl_android = linker_ns_dlopen("libdl_android.so", RTLD_LOCAL | RTLD_LAZY);
+    if(dl_android == NULL) {
+        dlclose(linkerhook);
+        dlclose(turnip_driver_handle);
+        return NULL;
+    }
+    void* android_get_exported_namespace = dlsym(dl_android, "android_get_exported_namespace");
+    void (*linkerhook_pass_handles)(void*, void*, void*) = dlsym(linkerhook, "app__pojav_linkerhook_pass_handles");
+    if(linkerhook_pass_handles == NULL || android_get_exported_namespace == NULL) {
+        dlclose(dl_android);
+        dlclose(linkerhook);
+        dlclose(turnip_driver_handle);
+        return NULL;
+    }
+    linkerhook_pass_handles(turnip_driver_handle, android_dlopen_ext, android_get_exported_namespace);
+    void* libvulkan = linker_ns_dlopen_unique(cache_dir, "libvulkan.so", RTLD_LOCAL | RTLD_NOW);
+    return libvulkan;
+}
+#endif
+
+static void set_vulkan_ptr(void* ptr) {
+    char envval[64];
+    sprintf(envval, "%"PRIxPTR, (uintptr_t)ptr);
+    setenv("VULKAN_PTR", envval, 1);
+}
+
+void load_vulkan() {
+    if(getenv("POJAV_ZINK_PREFER_SYSTEM_DRIVER") == NULL && android_get_device_api_level() >= 28) {
+    // the loader does not support below that
+#ifdef ADRENO_POSSIBLE
+        void* result = load_turnip_vulkan();
+        if(result != NULL) {
+            printf("AdrenoSupp: Loaded Turnip, loader address: %p\n", result);
+            set_vulkan_ptr(result);
+            return;
+        }
+#endif
+    }
+    printf("OSMDroid: loading vulkan regularly...\n");
+    void* vulkan_ptr = dlopen("libvulkan.so", RTLD_LAZY | RTLD_LOCAL);
+    printf("OSMDroid: loaded vulkan, ptr=%p\n", vulkan_ptr);
+    set_vulkan_ptr(vulkan_ptr);
+}
 
 GLFWbool _glfwInitOSMesa(void)
 {
@@ -243,6 +348,9 @@ GLFWbool _glfwCreateContextOSMesa(_GLFWwindow* window,
                                   const _GLFWfbconfig* fbconfig)
 {
     const char *renderer = getenv("LIBGL_STRING");
+    if (strcmp(renderer, "Zink") == 0) {
+        load_vulkan();
+    }
     if (strcmp(renderer, "VirGLRenderer") == 0) {
         EGLConfig config = malloc(sizeof(EGLConfig));
         EGLint egl_attributes[] = {
