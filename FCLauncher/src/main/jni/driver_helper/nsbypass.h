630ed1c5bebdebaf91ce7d5cdf17445fda6821c3
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2023-09-28 20:56:49 +0800

    Update

--- FCLauncher/src/main/jni/driver_helper/nsbypass.h
+++ FCLauncher/src/main/jni/driver_helper/nsbypass.h
@@ -3,8 +3,8 @@
 // Created by maks on 05.06.2023.
 //
 
-#ifndef POJAVLAUNCHER_NSBYPASS_H
-#define POJAVLAUNCHER_NSBYPASS_H
+#ifndef FCL_NSBYPASS_H
+#define FCL_NSBYPASS_H
 
 #include <stdbool.h>
 
@@ -12,4 +12,4 @@ bool linker_ns_load(const char* lib_search_path);
 void* linker_ns_dlopen(const char* name, int flag);
 void* linker_ns_dlopen_unique(const char* tmpdir, const char* name, int flag);
 
-#endif //POJAVLAUNCHER_NSBYPASS_H
+#endif //FCL_NSBYPASS_H
